53c1a2dc59ed8b46b04c554afbf559f62c6db9de
nuyo <lgy000405@gmail.com>
2024-11-25 13:16:17 +0900

    login_v3

--- starLight/src/components/MainPageCom/Login.jsx
+++ starLight/src/components/MainPageCom/Login.jsx
@@ -3,20 +3,20 @@ import "./Login.css"; // CSS 파일 가져오기
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <div className="profile-container">
-        <div className="avatar"></div>
-        <div className="profile-info">
+    <div className="login-login-container">
+      <div className="login-profile-container">
+        <div className="login-avatar"></div>
+        <div className="login-profile-info">
           <h2>Welcome Back!</h2>
           <p>Please login to your account</p>
         </div>
       </div>
-      <form className="login-form">
-        <div className="form-group">
+      <form className="login-login-form">
+        <div className="login-form-group">
           <label htmlFor="email">Email</label>
           <input type="email" id="email" placeholder="Enter your email" />
         </div>
-        <div className="form-group">
+        <div className="login-form-group">
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -24,7 +24,7 @@ const Login = () => {
             placeholder="Enter your password"
           />
         </div>
-        <button type="submit" className="login-button">
+        <button type="submit" className="login-login-button">
           Login
         </button>
       </form>
