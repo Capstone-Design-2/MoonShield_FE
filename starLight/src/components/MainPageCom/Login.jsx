a03ecdfafdf85ce9f492014b0cd030b4775476e6
nuyo <lgy000405@gmail.com>
2024-11-25 13:12:27 +0900

    login_v2

--- starLight/src/components/MainPageCom/Login.jsx
+++ starLight/src/components/MainPageCom/Login.jsx
@@ -1,7 +1,35 @@
-import "./Login.css";
+import React from "react";
+import "./Login.css"; // CSS 파일 가져오기
 
 const Login = () => {
-  return <div> login components</div>;
+  return (
+    <div className="login-container">
+      <div className="profile-container">
+        <div className="avatar"></div>
+        <div className="profile-info">
+          <h2>Welcome Back!</h2>
+          <p>Please login to your account</p>
+        </div>
+      </div>
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
